73f983f84a613b81c142982ced21c1e9ef3615b7
Pavel Cisar <pcisar@ibphoenix.cz>
2018-04-26 14:33:52 +0200

    New setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
 
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft :: Windows',
